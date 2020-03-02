9dc5e88340aecd43eb3c27179b3303a138c3374d
Andre Ribeiro  Claudio <andre.ribeiro.srs@gmail.com>
2020-03-02 19:40:48 -0300

    Learning

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import logging
 
 import keras
 import pandas as pd
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.models import Sequential
+from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 
 # Print in software terminal
 logging.basicConfig(level=logging.DEBUG,
@@ -17,12 +18,46 @@ logger = logging.getLogger(__name__)
 
 def application():
     """
-    System initialization.
-    :return: None
     """
     # All application has its initialization from here
     logger.info('Main application is running!')
+    cross_breast_cancer()
 
+
+def cross_breast_cancer():
+    """
+
+    """
+    predictors = pd.read_csv('entradas-breast.csv')
+    validation = pd.read_csv('saidas-breast.csv')
+
+    classifier = KerasClassifier(build_fn=generate_web, epochs=100, batch_size=10)
+    results = cross_val_score(estimator=classifier, X=predictors, y=validation, cv=10, scoring='accuracy')
+    mean = results.mean()
+    deviation = results.std()
+    return None
+
+
+def generate_web():
+    """
+
+    """
+    classifier = Sequential()
+    classifier.add(Dense(units=16, activation='relu', kernel_initializer='random_uniform', input_dim=30))
+    classifier.add(Dropout(0.2))
+    classifier.add(Dense(units=16, activation='relu', kernel_initializer='random_uniform'))
+    classifier.add(Dropout(0.2))
+    classifier.add(Dense(units=1, activation='sigmoid'))
+    optimizer = keras.optimizers.Adam(lr=0.001, decay=0.0001, clipvalue=0.5)
+    classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['binary_accuracy'])
+
+    return classifier
+
+
+def simple_breast_cancer():
+    """
+
+    """
     predictors = pd.read_csv('entradas-breast.csv')
     answers = pd.read_csv('saidas-breast.csv')
 
