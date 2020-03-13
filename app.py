c959438561e81061f453580bec9df988360d3b0c
Andre Ribeiro  Claudio <andre.ribeiro.srs@gmail.com>
2020-03-12 21:12:44 -0300

    Learning

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.preprocessing import LabelEncoder
 
-from .cars import autos
+from cars import autos
 
 # Print in software terminal
 logging.basicConfig(level=logging.DEBUG,
--- cars.py
+++ cars.py
@@ -15,3 +15,35 @@ def autos():
     base = base.drop('seller', axis=1)
     base['offerType'].value_counts()
     base = base.drop('offerType', axis=1)
+
+    i1 = base.loc[base.price <= 10]
+    base = base[base.price > 10]
+
+    i2 = base.loc[base.price > 350000]
+    base = base.loc[base.price < 350000]
+
+    # base.loc[pd.isnull(base['vehicleType'])]
+    # base['vehicleType'].value_counts()    # limousine
+
+    # base.loc[pd.isnull(base['gearbox'])]
+    # base['gearbox'].value_counts()  # manuell
+
+    # base.loc[pd.isnull(base['model'])]
+    # base['model'].value_counts()  # golf
+
+    # base.loc[pd.isnull(base['fuelType'])]
+    # base['fuelType'].value_counts()  # benzin
+
+    # base.loc[pd.isnull(base['notRepairedDamage'])]
+    # base['notRepairedDamage'].value_counts()  # nein
+
+    values = {'vehicleType': 'limousine',
+              'gearbox': 'manuell',
+              'model': 'golf',
+              'fuelType': 'benzin',
+              'notRepairedDamage': 'nein'
+              }
+
+    base = base.fillna(value=values)
+
+    return None
